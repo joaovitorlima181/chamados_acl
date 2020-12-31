83c677ae5cfdd445536b1a8d8aacb9aef1294e4d
João Vitor Lima <joaovitor9670@gmail.com>
2020-12-31 18:28:36 -0300

    Commit

--- resources/views/chamados/index.blade.php
+++ resources/views/chamados/index.blade.php
@@ -8,27 +8,29 @@
 
     <ul class="list-group">
 
-    @can('create', Chamado::class)  
+    @can('create', App\Http\Models\Chamado::class)
         <a href="/chamado/novo" class="btn btn-dark mb-2">Abrir Chamado</a>
     @endcan
 
+
+
     @forelse ($chamados as $chamado)
-       
+
     @can('view', $chamado)
         <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span id="tituloChamado-{{ $chamado->id }}">{{ $chamado->titulo }}</span>   
+        <span id="tituloChamado-{{ $chamado->id }}">{{ $chamado->titulo }}</span>
 
         @can('update', $chamado)
             <a href="/chamado/mostrar/{{$chamado->id}}" class="btn btn-dark mb-2">Editar</a>
         @endcan
 
-        
+
+        {{-- @can('delete', $chamado) --}}
         @can('delete', $chamado)
             <a href="/chamado/mostrar/{{$chamado->id}}" class="btn btn-danger mb-2">Deletar</a>
         @endcan
-        
+
     @endcan
-        
 
     @empty
         <h2>Nenhum Chamado.</h2>
